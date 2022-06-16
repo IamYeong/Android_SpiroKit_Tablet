b8ffdb413ce2d58ad70110ac6e33a54e7c879868
IamYeong <wjdrhkddud2@gmail.com>
2022-06-16 15:31:48 +0900

    Add patient insert activity

---
+++ app/src/main/java/kr/co/theresearcher/spirokitfortab/patient_input/PatientInsertActivity.java
@@ -0,0 +1,16 @@
+package kr.co.theresearcher.spirokitfortab.patient_input;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kr.co.theresearcher.spirokitfortab.R;
+
+public class PatientInsertActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_insert);
+    }
+}
---
+++ app/src/main/java/kr/co/theresearcher/spirokitfortab/patient_input/PatientModifyActivity.java
@@ -0,0 +1,18 @@
+package kr.co.theresearcher.spirokitfortab.patient_input;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kr.co.theresearcher.spirokitfortab.R;
+
+public class PatientModifyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_modify);
+
+
+    }
+}
